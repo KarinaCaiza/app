3e85a1df3df865008fce297dd3dcf6cb8a942b3d
KarinaCaiza <90169420+KarinaCaiza@users.noreply.github.com>
2021-09-06 08:37:39 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import lasio
+st.title("Aplicación para registro de pozos")
+st.sidebar.title("Menu")
+
+archivo_las=lasio.read("LGAE-040.las")
+df=archivo_las.df()
+
+archivo_las2=st.sidebar.file_uploader("Cargar archivo LAS" , type=['.las', '.LAS'], key=None)
+
+opciones_inicio=st.sidebar.radio("Seleccione una opción",["Inicio","Datos","Calculos"])
+if opciones_inicio=="Inicio":
+	st.write("Sección Inicio")
+	barra_deslizadora=st.slider("Seleccione un valor",1,100,30)
+	st.write(barra_deslizadora)
+	ingreso_numero=st.number_input("Ingrese un valor",min_value=0,max_value=1000,value=100)
+	st.write(ingreso_numero)
+	seleccion=st.selectbox("Seleccione una opción",["a","b","c"])
+	st.write(df)
+if opciones_inicio=="Datos":
+	st.write("Sección Datos")
+	st.write(df)
+if opciones_inicio=="Calculos":
+	st.write("Seccion calculos")
